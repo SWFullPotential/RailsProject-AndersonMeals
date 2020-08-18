814a525e02f3b8c35b22128516aaa3bd68bdb211
SWFullPotential <swfullpotential@gmail.com>
2020-08-17 18:13:58 -0600

    added views and actions for meals

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,15 @@
 class MealsController < ApplicationController
+    def new
+    end
+    def create
+    end
+    def edit 
+    end
+    def update 
+    end
+    def destroy 
+    end
+
+    private 
+    
 end
