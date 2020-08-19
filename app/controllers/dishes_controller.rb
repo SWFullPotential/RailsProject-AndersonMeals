755af9d0b536d94c2e57e9ef6c99b15ff242cc70
SWFullPotential <swfullpotential@gmail.com>
2020-08-18 20:44:47 -0600

    creating ability to add dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,2 +1,28 @@
 class DishesController < ApplicationController
+    def index 
+        @dishes = Dish.all
+    end
+    def new 
+        @dish = Dish.new 
+    end
+    def create 
+        
+    end
+    def show 
+    end
+    def edit 
+    end
+    def update 
+    end
+    def destroy 
+    end
+
+    private 
+
+    def dish_params 
+        params.require(:dish).permit(:dish_name)
+    end
+    def set_dish 
+        @dish = Dish.find_by(id: params[:id])
+    end
 end
