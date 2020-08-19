cad62b01cd3449c1e45e6e6126c025e3eaf40f53
SWFullPotential <swfullpotential@gmail.com>
2020-08-18 21:06:47 -0600

    moved route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'static#home'
-  resources :dishes
   resources :meals do 
     resources :dishes, only: [:index, :new, :create]
   end
+  resources :dishes
 
+  post 'meals/:meal_id/dishes' => 'dishes#create'
+
+
+  # post '/users/:id' => 'users#show_post_ride'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
