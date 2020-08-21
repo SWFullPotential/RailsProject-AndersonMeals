ca2494d23084d502574f37129ec8cff9a52d12b9
SWFullPotential <swfullpotential@gmail.com>
2020-08-21 12:24:59 -0600

    refactor to use helper

--- app/helpers/meals_helper.rb
+++ app/helpers/meals_helper.rb
@@ -1,2 +1,16 @@
 module MealsHelper
+
+    def add_edit_dish(meal)
+        if meal.dish 
+            content_tag :td do 
+                link_to meal.dish.dish_name, edit_dish_path(meal.dish_id)
+            end
+        else
+            content_tag :td do 
+                link_to "Add Dish", new_meal_dish_path(meal)
+            end 
+        end
+
+    end
+
 end
