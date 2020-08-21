56c914ac270b3925847b636a23687722768a6b61
SWFullPotential <swfullpotential@gmail.com>
2020-08-21 15:39:10 -0600

    remove averge user ability to edit  dish

--- app/helpers/meals_helper.rb
+++ app/helpers/meals_helper.rb
@@ -3,7 +3,7 @@ module MealsHelper
     def add_edit_dish(meal)
         if meal.dish 
             content_tag :td do 
-                link_to meal.dish.dish_name, edit_dish_path(meal.dish_id)
+                link_to meal.dish.dish_name, edit_meal_path(meal.id)
             end
         else
             content_tag :td do 
